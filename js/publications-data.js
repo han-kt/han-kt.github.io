0e884054cf2788659b455b29407da1393e7cbda5
KT Han <kt.han@toyota.com>
2025-08-22 20:08:14 -0700

    Update modern homepage 2025-08-22

--- js/publications-data.js
+++ js/publications-data.js
@@ -1,5 +1,5 @@
 // Modern Academic Homepage JavaScript
-// Generated on 2025-08-22 17:48:00
+// Generated on 2025-08-22 20:08:14
 // Total publications: 194
 // Breakdown: 66 conferences, 5 presentations, 19 journals, 102 patents
 
