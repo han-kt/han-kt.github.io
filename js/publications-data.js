745b4e5ac103028c1cc151a662afd82660f7dc0c
KT Han <108150053+han-kt@users.noreply.github.com>
2026-08-13 20:30:36 -0700

    Update modern homepage 2026-08-13

--- js/publications-data.js
+++ js/publications-data.js
@@ -1,5 +1,5 @@
 // Modern Academic Homepage JavaScript
-// Generated on 2026-08-07 18:22:43
+// Generated on 2026-08-13 20:30:34
 // Total publications: 225
 // Breakdown: 74 conferences, 8 presentations, 20 journals, 120 patents, 3 books
 
